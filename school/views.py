896f5d45ce7e8b16625edaa88822aea61daf3e24
riabow <63100171+riabov-vladimir@users.noreply.github.com>
2020-10-09 10:27:41 +0300

    prefetch_related - done (2 selects vs 4)

--- school/views.py
+++ school/views.py
@@ -4,14 +4,17 @@ from django.shortcuts import render
 from .models import Student
 
 
-class StudentsList(ListView):
-    model = Student
-    template = 'school/templates/school/student_list.html'
-    ordering = ['group']
-
-# def students_list_view(request):
+# class StudentsList(ListView):
+#     model = Student
 #     template = 'school/templates/school/student_list.html'
-#     context = {
-#
-#     }
-#     pass
+#     ordering = ['group']
+
+def students_list_view(request):
+    template = 'school/student_list.html'
+
+    students = Student.objects.prefetch_related('teacher').all()
+
+    context = {
+        'object_list': students.order_by('group')
+    }
+    return render(request, template, context)
